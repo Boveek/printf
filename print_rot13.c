085eebbb39cfd64305c2ac74fbc364801f9379c8
Boveek <oluwabori2014@gmail.com>
2023-04-25 09:14:53 -0700

    _printf update

--- print_rot13.c
+++ print_rot13.c
@@ -11,14 +11,13 @@ int print_rot13(va_list format)
 	char *ch;
 	char rec[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char ret[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int cnt, i = 0, val = 0;
+	int cnt, i = 0, val = 0, p = 0;
 
 	ch = va_arg(format, char *);
 
 	if (ch == NULL)
 		return (0);
-
-	while (ch)
+	while (ch[i] != '\0')
 	{
 		for (cnt = 0; rec[cnt] != '\0'; cnt++)
 		{
