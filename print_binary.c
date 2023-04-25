f81b412caed0e2ed188f28ecd8501845591920b4
phemmymoxie <gphemmy126@gmail.com>
2023-04-24 17:49:24 -0700

    printf

--- print_binary.c
+++ print_binary.c
@@ -11,27 +11,37 @@ int print_bin(va_list n)
 	char ch[32];
 	int i, val;
 	int count = 0;
-	int g;
+	int g, r, k = 0;
+	char *ch_out;
 
 	val = va_arg(n, int);
 	if (val)
 	{
 	for (i = 0; val > 0; i++)
 	{
-		ch[i] = (val % 2) + '0' ;
+		ch[i] = (val % 2) + '0';
 		val = val / 2;
 	}
-	
-	for (g = i; g < 32; g++)
-	{
-		ch[g] = '0';
-	}
 
-	for (g = i; g >= 0; g--)
+	ch_out = malloc(i + 1);
+
+	if (ch_out)
 	{
-		count = write(1, &ch[g], 1);
-		count += count;
+		ch_out[i + 1] = '\0';
+		while (*ch_out)
+		{
+			ch_out[k] = '0';
+			k++;
+		}
+		r = 0;
+		for (g = i; g >= 0; g--, r++)
+		{
+			ch_out[r] = ch[g];
+		}
+
 	}
+		count = write(1, ch_out, i + 1);
+		count += count;
 	}
 return (count);
 }
